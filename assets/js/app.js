a615c516dbebcee7c56bbd7545543c3cc29fe0ba
Semih Arslan <semihsemih@me.com>
2019-11-18 16:40:29 +0300

    attack buttons and attack logic prepared

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,48 @@
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    heroHealth: 100,
+    monsterHealth: 100,
+    heroAttackMultiplier: 10,
+    heroSpecialAttackMultiplier: 25,
+    heroHealUpMultiplier: 20,
+    monsterAttackMultiplier: 15,
+    gameIsOn: false
+  },
+  methods: {
+    startGame: function () {
+      this.gameIsOn = true;
+    },
+    randomPoint: function (multiplier) {
+      return Math.ceil(Math.random() * multiplier);
+    },
+    attack: function () {
+      const point = this.randomPoint(this.heroAttackMultiplier);
+      this.monsterHealth -= point;
+      this.monsterAttack();
+
+    },
+    specialAttack: function () {
+      const point = this.randomPoint(this.heroSpecialAttackMultiplier);
+      this.monsterHealth -= point;
+      this.monsterAttack();
+    },
+    healUp: function () {
+      const point = this.randomPoint(this.heroHealUpMultiplier);
+      this.heroHealth += point;
+      console.log('Hero: ' + this.heroHealth)
+      console.log('Monster: ' + this.monsterHealth)
+    },
+    giveUp: function() {
+      this.heroHealth = 0;
+      console.log('Hero: ' + this.heroHealth)
+      console.log('Monster: ' + this.monsterHealth)
+    },
+    monsterAttack: function () {
+      const point = this.randomPoint(this.monsterAttackMultiplier);
+      this.heroHealth -= point;
+      console.log('Hero: ' + this.heroHealth)
+      console.log('Monster: ' + this.monsterHealth)
+    }
+  }
 });
